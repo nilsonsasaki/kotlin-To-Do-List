b07f6483331cd59a9779f5d4133418bad18658fa
nilsonsasaki <nilsonsasaki@gmail.com>
2021-08-24 15:50:29 -0300

    added fragment support

---
+++ app/src/main/java/com/nilsonsasaki/kotlin_to_do_list/ui/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.nilsonsasaki.kotlin_to_do_list.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.nilsonsasaki.kotlin_to_do_list.R
+import com.nilsonsasaki.kotlin_to_do_list.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
