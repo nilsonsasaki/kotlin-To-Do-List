165be0e038a3b1751839b45ffd9dfca426b44590
nilsonsasaki <nilsonsasaki@gmail.com>
2021-08-24 15:52:41 -0300

    created task details fragment

---
+++ app/src/main/java/com/nilsonsasaki/kotlin_to_do_list/ui/TaskDetails.kt
@@ -0,0 +1,30 @@
+package com.nilsonsasaki.kotlin_to_do_list.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nilsonsasaki.kotlin_to_do_list.databinding.FragmentTaskDetailsBinding
+
+class TaskDetails : Fragment() {
+
+    private var _binding: FragmentTaskDetailsBinding?=null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentTaskDetailsBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
