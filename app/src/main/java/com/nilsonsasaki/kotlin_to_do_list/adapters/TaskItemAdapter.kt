498eed4e1c8711e1dc961328a68ab7199e1d894a
nilsonsasaki <nilsonsasaki@gmail.com>
2021-08-25 08:05:45 -0300

    created files

---
+++ app/src/main/java/com/nilsonsasaki/kotlin_to_do_list/adapters/TaskItemAdapter.kt
@@ -0,0 +1,4 @@
+package com.nilsonsasaki.kotlin_to_do_list.adapters
+
+class TaskItemAdapter {
+}
---
+++ app/src/main/java/com/nilsonsasaki/kotlin_to_do_list/ui/models/TaskViewModel.kt
@@ -0,0 +1,7 @@
+package com.nilsonsasaki.kotlin_to_do_list.ui.models
+
+import androidx.lifecycle.ViewModel
+
+class TaskViewModel : ViewModel() {
+
+}
