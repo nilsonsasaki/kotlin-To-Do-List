ea7676d692fc73494d0f141f3e9d41994b4db914
nilsonsasaki <nilsonsasaki@gmail.com>
2021-08-30 14:46:10 -0300

    created TaskViewModel

--- app/src/main/java/com/nilsonsasaki/kotlin_to_do_list/ui/models/TaskViewModel.kt
+++ app/src/main/java/com/nilsonsasaki/kotlin_to_do_list/ui/models/TaskViewModel.kt
@@ -1,7 +1,30 @@
 package com.nilsonsasaki.kotlin_to_do_list.ui.models
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.nilsonsasaki.kotlin_to_do_list.database.Task
+import com.nilsonsasaki.kotlin_to_do_list.database.TaskDao
+import kotlinx.coroutines.flow.Flow
 
-class TaskViewModel : ViewModel() {
+class TaskViewModel(private val taskDao: TaskDao) : ViewModel() {
 
+    fun getAllTasks(): Flow<List<Task>> = taskDao.getAllTasks()
+
+    fun getByDate(itemDate: String): Flow<List<Task>> = taskDao.getByDate(itemDate)
+
+    fun getAllByPriority(): Flow<List<Task>> = taskDao.getAllByPriority()
+
+}
+
+class TaskViewModelFactory(
+    private val taskDao: TaskDao
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TaskViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return TaskViewModel(taskDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
