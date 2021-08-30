29155ce64416c5b4f61b973039a2bf2edd181459
nilsonsasaki <nilsonsasaki@gmail.com>
2021-08-30 14:45:34 -0300

    created TaskItemAdapter

--- app/src/main/java/com/nilsonsasaki/kotlin_to_do_list/adapters/TaskItemAdapter.kt
+++ app/src/main/java/com/nilsonsasaki/kotlin_to_do_list/adapters/TaskItemAdapter.kt
@@ -1,4 +1,59 @@
 package com.nilsonsasaki.kotlin_to_do_list.adapters
 
-class TaskItemAdapter {
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.nilsonsasaki.kotlin_to_do_list.database.Task
+import com.nilsonsasaki.kotlin_to_do_list.databinding.TaskItemBinding
+
+
+class TaskItemAdapter(
+    private val onItemClicked: (Task) -> Unit
+) : ListAdapter<Task, TaskItemAdapter.TaskViewHolder>(DiffCallback) {
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<Task>() {
+            override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    class TaskViewHolder(private var binding: TaskItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SetTextI18n")
+        fun bind(task: Task) {
+            binding.tvTaskItemTitle.text = task.title
+            binding.tvDate.text = task.date
+            binding.tvItemStartingToEndTime.text = "${task.startingTime} to ${task.endingTime}"
+            binding.tvPriority.text = task.priority
+        }
+
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val viewHolder = TaskViewHolder(
+            TaskItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+        viewHolder.itemView.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            onItemClicked(getItem(position))
+        }
+        return viewHolder
+    }
 }
