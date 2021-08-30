75d6af6c84c96bcc80458002f504b932d4296081
nilsonsasaki <nilsonsasaki@gmail.com>
2021-08-30 14:34:52 -0300

    created AppDatabase

---
+++ app/src/main/java/com/nilsonsasaki/kotlin_to_do_list/database/AppDatabase.kt
@@ -0,0 +1,32 @@
+package com.nilsonsasaki.kotlin_to_do_list.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "app_database"
+                )
+                    .createFromAsset("database/tasks.db")
+                    .build()
+                INSTANCE = instance
+
+                instance
+            }
+        }
+    }
+}
