a8a6ba46c913b033b0abd495fc92e9c1e84b11c3
nilsonsasaki <nilsonsasaki@gmail.com>
2021-09-15 13:22:36 -0300

    added date and time format functions

--- app/src/main/java/com/nilsonsasaki/kotlin_to_do_list/extensions/AppExtensions.kt
+++ app/src/main/java/com/nilsonsasaki/kotlin_to_do_list/extensions/AppExtensions.kt
@@ -2,13 +2,17 @@ package com.nilsonsasaki.kotlin_to_do_list.extensions
 
 
 import com.google.android.material.textfield.TextInputLayout
-import java.text.SimpleDateFormat
-import java.util.*
 
-private val locale = Locale ("pt","BR")
+fun dateFormat(day:Int,month:Int,year:Int):String{
+    val dayString = if (day in 1..9)" $day" else "$day"
+    val monthString = if(month in 1..9)"0$month" else "$month"
+    return "$dayString/$monthString/$year"
+}
 
-fun Date.format():String{
-    return SimpleDateFormat("dd/mm/yyyy",locale).format(this)
+fun timeFormat(hour:Int,minute:Int):String{
+    val hourString = if(hour in 0..9) "0$hour" else "$hour"
+    val minuteString = if (minute in 0..9) "0$minute" else "$minute"
+    return "$hourString:$minuteString"
 }
 
 var TextInputLayout.text : String
